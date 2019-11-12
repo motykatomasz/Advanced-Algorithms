bfd38501f09fb474b415c767725b9565b6938edb
rpytel1 <rafal.pytel1@wp.pl>
2019-11-12 16:28:37 +0100

    [Assignment 2] working subset creation

--- Assignement 1/src/main/java/ExactSolver.java
+++ Assignement 1/src/main/java/ExactSolver.java
@@ -1,18 +1,37 @@
-import java.lang.reflect.Array;
 import java.util.*;
 
 public class ExactSolver implements Solver {
 
+    private static List<List<Integer>> generateSubsets(List<Integer> set) {
+        Set<List<Integer>> subsets = new HashSet<>();
+        double counter = Math.pow(2, set.size());
+
+        for (int i = 0; i < counter; i++) {
+            List<Integer> candidateSet = new ArrayList<>();
+            String str = String.format("%" + set.size() + "s", Integer.toBinaryString(i)).replaceAll(" ", "0");
+
+            for (int j = 0; j < str.length(); j++) {
+                char letter = str.charAt(j);
+                if (letter == '1') {
+                    candidateSet.add(set.get(j));
+                }
+            }
+            subsets.add(candidateSet);
+        }
+
+        return new ArrayList<>(subsets);
+    }
+
     private int getOptimalValue(AuctionProblemInstance a) {
         //Initialize structures
-        Set<Integer> initialSet = new HashSet<>();
+        List<Integer> initialSet = new ArrayList<>();
         for (int i = 0; i < a.k; i++) {
             initialSet.add(i);
         }
-        Map<Set<Integer>, List<PartialSolution>> structure = initializeStructure(initialSet);
+        Map<List<Integer>, List<PartialSolution>> structure = initializeStructure(initialSet);
 
         //Fill out the first row
-        for (Map.Entry<Set<Integer>, List<PartialSolution>> entry : structure.entrySet()) {
+        for (Map.Entry<List<Integer>, List<PartialSolution>> entry : structure.entrySet()) {
             int[] assignment = new int[a.k];
             Arrays.fill(assignment, -1);
             for (Integer i : entry.getKey()) {
@@ -24,10 +43,10 @@ public class ExactSolver implements Solver {
         }
 
         for (int i = 1; i < a.n; i++) {
-            for (Map.Entry<Set<Integer>, List<PartialSolution>> entry : structure.entrySet()) {
+            for (Map.Entry<List<Integer>, List<PartialSolution>> entry : structure.entrySet()) {
 
                 List<PartialSolution> maxPartialSolution = new ArrayList<>();
-                for (Set<Integer> subset : generateSubsets(entry.getKey())) {
+                for (List<Integer> subset : generateSubsets(entry.getKey())) {
                     Set<Integer> complementSubset = new HashSet(entry.getKey());
                     complementSubset.removeAll(subset);
 
@@ -45,26 +64,21 @@ public class ExactSolver implements Solver {
             }
         }
 
-        return structure.get(initialSet).get(a.n-1).getRevenue();
+        return structure.get(initialSet).get(a.n - 1).getRevenue();
     }
 
-    private Map<Set<Integer>, List<PartialSolution>> initializeStructure(Set<Integer> initialSet) {
-        Map<Set<Integer>, List<PartialSolution>> structure = new HashMap<>();
+    private Map<List<Integer>, List<PartialSolution>> initializeStructure(List<Integer> initialSet) {
+        Map<List<Integer>, List<PartialSolution>> structure = new HashMap<>();
 
-        for (Set<Integer> subset : generateSubsets(initialSet)) {
+        for (List<Integer> subset : generateSubsets(initialSet)) {
             structure.put(subset, new ArrayList<PartialSolution>());
         }
 
         return structure;
     }
 
-    private Set<Set<Integer>> generateSubsets(Set<Integer> set) {
-
-        return null;
-    }
-
     private int evaluate(int[] assignment, AuctionProblemInstance a) {
-        
+
         return 0;
     }
 
