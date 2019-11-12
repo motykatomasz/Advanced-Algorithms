b6301acc687b617c243fb021ceecb69daa4ad36c
rpytel1 <rafal.pytel1@wp.pl>
2019-11-12 15:32:11 +0100

    [Assignment 1] Test generator improved

--- Assignement 1/src/main/java/TestCasesGenerator.java
+++ Assignement 1/src/main/java/TestCasesGenerator.java
@@ -1,25 +1,36 @@
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class TestCasesGenerator {
     public static void main(String[] args) {
-        measure("./test_d/", 0.5, null);
+        generateForEps();
+        System.out.println("Genererated");
+        double[] eps = {0.1, 0.2, 0.3, 0.4, 0.5};
+        measure("./test_eps/", 0.4, eps);
     }
 
     static void measure(String pathToDirectory, double epsilon, double[] epsilons) {
         if (epsilons == null)
             measure(pathToDirectory, epsilon);
         else {
-            for (double eps : epsilons)
+            System.out.println(epsilons[1]);
+            for (double eps : epsilons) {
+                System.out.println(eps);
                 measure(pathToDirectory, eps);
+            }
         }
     }
 
     static void measure(String pathToDirectory, double epsilon) {
         File dir = new File(pathToDirectory);
         File[] directoryListing = dir.listFiles();
+        List<File> directoryList = Arrays.stream(directoryListing).sorted((Comparator.comparing(File::getName))).collect(Collectors.toList());
         if (directoryListing != null) {
-            for (File child : directoryListing) {
+            for (File child : directoryList) {
                 try {
                     System.out.println(new FPTASSolver().solve(AuctionProblemInstance.IO.read(child), epsilon).value);
                 } catch (IOException e) {
@@ -32,7 +43,7 @@ public class TestCasesGenerator {
     static void generateForK() {
         int n = 5;
         int d = 400;
-        int[] k = {2, 5, 10, 15, 20, 30};
+        int[] k = {2, 5, 7, 10, 15, 20};
 
         for (int i = 0; i < k.length; i++) {
             String test_k = "Test_n_" + n + "_d_" + d + "_k_";
@@ -48,15 +59,16 @@ public class TestCasesGenerator {
     static void generateForN() {
         int[] n = {2, 4, 6, 8, 10, 12};
         int d = 400;
-        int k = 10;
-
-        for (int i = 0; i < n.length; i++) {
-            String test_n = "Test_k_" + k + "_d_" + d + "_n_";
-            try {
-                AuctionProblemInstance.IO.save(AuctionProblemInstance.generate(n[i], k, d),
-                        System.getProperty("user.dir") + "\\test_n\\" + test_n + n[i] + ".txt");
-            } catch (IOException e) {
-                System.out.println("Something went wrong");
+        int k = 5;
+        for (int j = 0; j < 10; j++) {
+            for (int i = 0; i < n.length; i++) {
+                String test_n = "Test_k_" + k + "_d_" + d + "_n_";
+                try {
+                    AuctionProblemInstance.IO.save(AuctionProblemInstance.generate(n[i], k, d),
+                            System.getProperty("user.dir") + "/test_n/" + test_n + n[i] + j + ".txt");
+                } catch (IOException e) {
+                    System.out.println("Something went wrong");
+                }
             }
         }
     }
@@ -65,34 +77,35 @@ public class TestCasesGenerator {
         int n = 5;
         int[] d = {100, 200, 400, 800, 1000, 2000};
         int k = 10;
-
-        for (int i = 0; i < d.length; i++) {
-            String test_d = "Test_k_" + k + "_n_" + n + "_d_";
-            try {
-                AuctionProblemInstance.IO.save(AuctionProblemInstance.generate(n, k, d[i]),
-                        System.getProperty("user.dir") + "\\test_d\\" + test_d + d[i] + ".txt");
-            } catch (IOException e) {
-                System.out.println("Something went wrong");
+        for (int j = 0; j < 5; j++) {
+            for (int i = 0; i < d.length; i++) {
+                String test_d = "Test_k_" + k + "_n_" + n + "_d_";
+                try {
+                    AuctionProblemInstance.IO.save(AuctionProblemInstance.generate(n, k, d[i]),
+                            System.getProperty("user.dir") + "/test_d/" + test_d + d[i] + j + ".txt");
+                } catch (IOException e) {
+                    System.out.println("Something went wrong");
+                }
             }
         }
     }
 
     static void generateForEps() {
-        int n = 10;
+        int n = 7;
         int d = 400;
         double[] eps = {0.1, 0.2, 0.3, 0.4, 0.5};
-        int k = 10;
+        int k = 8;
+        for (int j = 0; j < 10; j++) {
 
-        for (int i = 0; i < eps.length; i++) {
             String test_eps = "Test_k_" + k + "_n_" + n + "_d_" + d + "_eps_";
             try {
                 AuctionProblemInstance.IO.save(AuctionProblemInstance.generate(n, k, d),
-                        System.getProperty("user.dir") + "\\test_eps\\" + test_eps + eps[i] + ".txt");
+                        System.getProperty("user.dir") + "/test_eps/" + test_eps + j + ".txt");
             } catch (IOException e) {
                 System.out.println("Something went wrong");
             }
         }
-    }
 
+    }
 
 }
