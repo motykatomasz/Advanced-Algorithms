fb0666a0a635647f613de545e14132dc03f0de90
Tomasz <motyka.tomasz.1995@gmail.com>
2020-01-11 14:33:15 +0100

    [Assignment 3] Add tic-tocs :)

--- Assignment 3/maxcut.m
+++ Assignment 3/maxcut.m
@@ -1,14 +1,21 @@
-w = read_graph("large.txt")
+clear;
+clc;
+w = read_graph("data/10/maxcut_10_05_100_instance_01.txt")
 n=length(w);
 yalmip('clear');
 Y=sdpvar(n,n);
 constraints=[Y>=0; diag(Y)==ones(n,1)];
 objective=-trace(w*(ones(n,n)-Y))/4;
+tic
 maxcutsol=solvesdp(constraints,objective);
+toc
 
+tic
 [Q, A] = eig(value(Y));
 B = Q * sqrt(A);
 B = B';
+toc
+
 while 1
     r = normrnd(0,1,[1,n]);
     r = r./norm(r);
